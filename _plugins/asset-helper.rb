444519b2972a8ec79d9a853f8c8aa017d3675d37
HarikaMunipalle <harika.munipalle@gsa.gov>
2023-11-02 18:46:28 -0400

    commit for 10641

--- _plugins/asset-helper.rb
+++ _plugins/asset-helper.rb
@@ -3,9 +3,9 @@ require 'fileutils'
 module AssetHelper
     def self.process(site, payload)
         return if @processed
-        FileUtils.cp_r('node_modules/uswds/dist/js/', 'assets/uswds/', verbose: true)
-        FileUtils.cp_r('node_modules/uswds/dist/img/', 'assets/uswds/', verbose: true)
-        FileUtils.cp_r('node_modules/uswds/dist/fonts/', 'assets/uswds/', verbose: true)
+        FileUtils.cp_r('node_modules/uswds/dist/js/', 'assets/', verbose: true)
+        FileUtils.cp_r('node_modules/uswds/dist/img/', 'assets/', verbose: true)
+        FileUtils.cp_r('node_modules/uswds/dist/fonts/', 'assets/', verbose: true)
         @processed = true
     end
 end
