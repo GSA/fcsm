e98b397d23c27a69987266307fccfd47c8457469
arnayv-47 <arnay.vohra@gsa.gov>
2025-05-22 14:16:16 -0400

    update mean menu

--- assets/js/jquery.meanmenu.js
+++ assets/js/jquery.meanmenu.js
@@ -1,19 +1,7 @@
 !function($) {
     "use strict";
-    function escapeHtml(str) {
-        return str.replace(/[&<>"']/g, function(m) {
-            return ({
-                '&': '&amp;',
-                '<': '&lt;',
-                '>': '&gt;',
-                '"': '&quot;',
-                "'": '&#39;'
-            })[m];
-        });
-    }
-
-    $.fn.meanmenu = function(e) {
-        var n = {
+    $.fn.meanmenu = function(options) {
+        var defaults = {
             meanMenuTarget: jQuery(this),
             meanMenuContainer: ".spacle-responsive-menu",
             meanMenuClose: "X",
@@ -24,145 +12,180 @@
             meanRevealColour: "",
             meanScreenWidth: "480",
             meanNavPush: "",
-            meanShowChildren: !0,
-            meanExpandableChildren: !0,
+            meanShowChildren: true,
+            meanExpandableChildren: true,
             meanExpand: "+",
             meanContract: "-",
-            meanRemoveAttrs: !1,
-            onePage: !1,
+            meanRemoveAttrs: false,
+            onePage: false,
             meanDisplay: "block",
             removeElements: ""
         };
-        e = $.extend(n, e);
-        var a = window.innerWidth || document.documentElement.clientWidth;
+        options = $.extend(defaults, options);
+        var screenWidth = window.innerWidth || document.documentElement.clientWidth;
         return this.each(function() {
-            var n = e.meanMenuTarget,
-                t = e.meanMenuContainer,
-                r = e.meanMenuClose,
-                i = e.meanMenuCloseSize,
-                s = e.meanMenuOpen,
-                u = e.meanRevealPosition,
-                m = e.meanRevealPositionDistance,
-                l = e.meanRevealColour,
-                o = e.meanScreenWidth,
-                c = e.meanNavPush,
-                v = ".meanmenu-reveal",
-                h = e.meanShowChildren,
-                d = e.meanExpandableChildren,
-                y = e.meanExpand,
-                j = e.meanContract,
-                Q = e.meanRemoveAttrs,
-                f = e.onePage,
-                g = e.meanDisplay,
-                p = e.removeElements,
-                C = !1;
-
-            (navigator.userAgent.match(/iPhone|iPod|iPad|Android|Blackberry|Windows Phone/i)) && (C = !0);
-            (navigator.userAgent.match(/MSIE 8|MSIE 7/i)) && jQuery("html").css("overflow-y", "scroll");
-
-            var w = "",
-                x = function() {
-                    if ("center" === u) {
-                        var e = window.innerWidth || document.documentElement.clientWidth,
-                            n = e / 2 - 22 + "px";
-                        w = "left:" + n + ";right:auto;";
-                        C ? jQuery(".meanmenu-reveal").animate({ left: n }) : jQuery(".meanmenu-reveal").css("left", n);
+            var means = options,
+                n = means.meanMenuTarget,
+                t = means.meanMenuContainer,
+                closeStr = means.meanMenuClose,
+                closeSize = means.meanMenuCloseSize,
+                openStr = means.meanMenuOpen,
+                revealPos = means.meanRevealPosition,
+                revealDist = means.meanRevealPositionDistance,
+                revealColor = means.meanRevealColour,
+                maxWidth = means.meanScreenWidth,
+                navPush = means.meanNavPush,
+                showChildren = means.meanShowChildren,
+                expandableChildren = means.meanExpandableChildren,
+                expandStr = means.meanExpand,
+                contractStr = means.meanContract,
+                removeAttrs = means.meanRemoveAttrs,
+                singlePage = means.onePage,
+                displayStyle = means.meanDisplay,
+                removeEl = means.removeElements;
+            var isMobile = navigator.userAgent.match(/iPhone|iPod|iPad|Android|Blackberry|Windows Phone/i),
+                isOldIE = navigator.userAgent.match(/MSIE 8|MSIE 7/i);
+            if (isOldIE) {
+                jQuery("html").css("overflow-y", "scroll");
+            }
+            var revealStyles = "",
+                positionReveal = function() {
+                    if (revealPos === "center") {
+                        var w = window.innerWidth || document.documentElement.clientWidth,
+                            leftPos = w / 2 - 22 + "px";
+                        revealStyles = "left:" + leftPos + ";right:auto;";
+                        if (isMobile) {
+                            jQuery(".meanmenu-reveal").animate({ left: leftPos });
+                        } else {
+                            jQuery(".meanmenu-reveal").css("left", leftPos);
+                        }
                     }
                 },
-                A = !1,
-                E = !1;
-
-            "right" === u && (w = "right:" + m + ";left:auto;");
-            "left" === u && (w = "left:" + m + ";right:auto;");
-            x();
-
-            var M = "",
-                P = function() {
-                    M.html(jQuery(M).is(".meanmenu-reveal.meanclose") ? r : s);
+                menuOpen = false,
+                menuBuilt = false;
+            if (revealPos === "right") {
+                revealStyles = "right:" + revealDist + ";left:auto;";
+            }
+            if (revealPos === "left") {
+                revealStyles = "left:" + revealDist + ";right:auto;";
+            }
+            positionReveal();
+            var revealButton,
+                toggleRevealText = function() {
+                    revealButton.html(jQuery(revealButton).is(".meanmenu-reveal.meanclose") ? closeStr : openStr);
                 },
-                W = function() {
-                    jQuery(".mean-bar,.mean-push").remove();
+                removeMean = function() {
+                    jQuery(".mean-bar, .mean-push").remove();
                     jQuery(t).removeClass("mean-container");
-                    jQuery(n).css("display", g);
-                    A = !1;
-                    E = !1;
-                    jQuery(p).removeClass("mean-remove");
+                    jQuery(n).css("display", displayStyle);
+                    menuOpen = false;
+                    menuBuilt = false;
+                    jQuery(removeEl).removeClass("mean-remove");
                 },
-                b = function() {
-                    var e = "background:" + l + ";color:" + l + ";" + w;
-                    if (o >= a) {
-                        jQuery(p).addClass("mean-remove");
-                        E = !0;
-                        jQuery(t).addClass("mean-container");
-                        jQuery(".mean-container").prepend('<div class="mean-bar"><a href="#nav" class="meanmenu-reveal" style="' + e + '">Show Navigation</a><nav class="mean-nav"></nav></div>');
-                        var r = escapeHtml(jQuery(n).html() || '');
-                        jQuery(".mean-nav").html(r);
-                        Q && jQuery("nav.mean-nav ul, nav.mean-nav ul *").each(function() {
-                            jQuery(this).is(".mean-remove") ? jQuery(this).attr("class", "mean-remove") : jQuery(this).removeAttr("class");
-                            jQuery(this).removeAttr("id");
-                        });
-                        jQuery(n).before('<div class="mean-push" />');
-                        jQuery(".mean-push").css("margin-top", c);
+                buildMean = function() {
+                    if (maxWidth >= screenWidth) {
+                        jQuery(removeEl).addClass("mean-remove");
+                        menuBuilt = true;
+                        var $container = jQuery(t).addClass("mean-container");
+                        var $bar = jQuery("<div>", { class: "mean-bar" });
+                        var $link = jQuery("<a>", {
+                            href: "#nav",
+                            class: "meanmenu-reveal"
+                        }).html(openStr).attr("style", "background:" + revealColor + ";color:" + revealColor + ";" + revealStyles);
+                        var $nav = jQuery("<nav>", { class: "mean-nav" });
+                        $bar.append($link, $nav);
+                        $container.prepend($bar);
+                        var $push = jQuery("<div>", { class: "mean-push" }).css("margin-top", navPush);
+                        jQuery(n).before($push);
+                        var $menuClone = jQuery(n).clone();
+                        $nav.append($menuClone.children());
                         jQuery(n).hide();
                         jQuery(".meanmenu-reveal").show();
-                        jQuery(v).html(s);
-                        M = jQuery(v);
+                        revealButton = jQuery(".meanmenu-reveal");
                         jQuery(".mean-nav ul").hide();
-                        h ? d ? (
-                            jQuery(".mean-nav ul ul").each(function() {
-                                jQuery(this).children().length && jQuery(this, "li:first").parent().append('<a class="mean-expand" href="#" style="font-size: ' + i + '">' + y + "</a>");
-                            }),
-                            jQuery(".mean-expand").on("click", function(e) {
-                                e.preventDefault();
-                                jQuery(this).hasClass("mean-clicked") ? (
-                                    jQuery(this).text(y),
-                                    jQuery(this).prev("ul").slideUp(300)
-                                ) : (
-                                    jQuery(this).text(j),
-                                    jQuery(this).prev("ul").slideDown(300)
-                                );
-                                jQuery(this).toggleClass("mean-clicked");
-                            })
-                        ) : jQuery(".mean-nav ul ul").show() : jQuery(".mean-nav ul ul").hide();
+                        if (showChildren) {
+                            if (expandableChildren) {
+                                jQuery(".mean-nav ul ul").each(function() {
+                                    if (jQuery(this).children().length) {
+                                        var $expander = jQuery("<a>", { class: "mean-expand", href: "#" }).text(expandStr).css("font-size", closeSize);
+                                        jQuery(this).parent().append($expander);
+                                    }
+                                });
+                                jQuery(document).on("click", ".mean-expand", function(e) {
+                                    e.preventDefault();
+                                    var $exp = jQuery(this);
+                                    if ($exp.hasClass("mean-clicked")) {
+                                        $exp.text(expandStr);
+                                        $exp.prev("ul").slideUp(300);
+                                    } else {
+                                        $exp.text(contractStr);
+                                        $exp.prev("ul").slideDown(300);
+                                    }
+                                    $exp.toggleClass("mean-clicked");
+                                });
+                            } else {
+                                jQuery(".mean-nav ul ul").show();
+                            }
+                        } else {
+                            jQuery(".mean-nav ul ul").hide();
+                        }
                         jQuery(".mean-nav ul li").last().addClass("mean-last");
-                        M.removeClass("meanclose");
-                        jQuery(M).click(function(e) {
+                        revealButton.removeClass("meanclose");
+                        revealButton.on("click", function(e) {
                             e.preventDefault();
-                            A === !1 ? (
-                                M.css({ "text-align": "center", "text-indent": "0", "font-size": i }),
-                                jQuery(".mean-nav ul:first").slideDown(),
-                                A = !0
-                            ) : (
-                                jQuery(".mean-nav ul:first").slideUp(),
-                                A = !1
-                            );
-                            M.toggleClass("meanclose");
-                            P();
-                            jQuery(p).addClass("mean-remove");
+                            if (!menuOpen) {
+                                revealButton.css({ "text-align": "center", "text-indent": "0", "font-size": closeSize });
+                                jQuery(".mean-nav ul:first").slideDown();
+                                menuOpen = true;
+                            } else {
+                                jQuery(".mean-nav ul:first").slideUp();
+                                menuOpen = false;
+                            }
+                            revealButton.toggleClass("meanclose");
+                            toggleRevealText();
+                            jQuery(removeEl).addClass("mean-remove");
                         });
-                        f && jQuery(".mean-nav ul > li > a:first-child").on("click", function() {
-                            jQuery(".mean-nav ul:first").slideUp();
-                            A = !1;
-                            jQuery(M).toggleClass("meanclose").html(s);
-                        });
+                        if (singlePage) {
+                            jQuery(".mean-nav ul > li > a:first-child").on("click", function() {
+                                jQuery(".mean-nav ul:first").slideUp();
+                                menuOpen = false;
+                                revealButton.toggleClass("meanclose").html(openStr);
+                            });
+                        }
                     } else {
-                        W();
+                        removeMean();
                     }
                 };
-
-            C || jQuery(window).resize(function() {
-                a = window.innerWidth || document.documentElement.clientWidth;
-                a > o;
-                W();
-                o >= a ? (b(), x()) : W();
-            });
-
+            if (!isMobile) {
+                jQuery(window).resize(function() {
+                    screenWidth = window.innerWidth || document.documentElement.clientWidth;
+                    removeMean();
+                    if (maxWidth >= screenWidth) {
+                        buildMean();
+                        positionReveal();
+                    } else {
+                        removeMean();
+                    }
+                });
+            }
             jQuery(window).resize(function() {
-                a = window.innerWidth || document.documentElement.clientWidth;
-                C ? (x(), o >= a ? E === !1 && b() : W()) : (W(), o >= a && (b(), x()));
+                screenWidth = window.innerWidth || document.documentElement.clientWidth;
+                if (isMobile) {
+                    positionReveal();
+                    if (maxWidth >= screenWidth && !menuBuilt) {
+                        buildMean();
+                    } else {
+                        removeMean();
+                    }
+                } else {
+                    removeMean();
+                    if (maxWidth >= screenWidth) {
+                        buildMean();
+                        positionReveal();
+                    }
+                }
             });
-
-            b();
+            buildMean();
         });
-    }
+    };
 }(jQuery);
